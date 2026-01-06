4eb7b1cee37c91acebb22c31c3c5dfedcd4c2d53
Akilesh A S <akileshas.ai@gmail.com>
2026-01-06 23:44:21 +0530

    test: init hooks test

---
+++ tests/server-hooks.test.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.extend([
+  os.path.abspath(os.path.join(os.path.dirname(__file__), ".")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),
+])
+
+from rich import print
+from hooks import server
