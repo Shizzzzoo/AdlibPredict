2ab5d4579683f07b8974dc07934624051b551d78
Akilesh A S <akileshas.ai@gmail.com>
2026-01-06 12:25:41 +0530

    feat: update trainer

--- model/train.py
+++ model/train.py
@@ -1,3 +1,27 @@
+import os
+import sys
+
 from ultralytics import YOLO
 
-model = YOLO("yolo11.yaml")
+COCO_DATASET = os.path.abspath(os.path.join(
+  os.path.dirname(__file__),
+  "./dataset/coco/data.yaml",
+))
+WEIGHTS_PATH = os.path.abspath(os.path.join(
+  os.path.dirname(__file__),
+  "./weights/"
+))
+YOLO11M = os.path.abspath(os.path.join(
+  WEIGHTS_PATH,
+  "pretrained/yolo11m.pt"
+))
+
+model = YOLO(YOLO11M)
+model.train(
+  data=COCO_DATASET,
+  epochs=500,
+  imgsz=1024,
+  plots=True,
+  project=WEIGHTS_PATH,
+  name="trainOne",
+)
