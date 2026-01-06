0b1eda1fa67a65e074772b5647193794e26a5252
Akilesh A S <akileshas.ai@gmail.com>
2026-01-06 12:04:28 +0530

    fix: update weight path

--- tests/model-predict.test.py
+++ tests/model-predict.test.py
@@ -8,7 +8,7 @@ root = os.path.dirname(__file__)
 model = YOLO(
   os.path.join(
     root,
-    "../adlibpredict/weights/best.pt",
+    "../model/weights/best.pt",
   )
 )
 input_dir = os.path.join(
