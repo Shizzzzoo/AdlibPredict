633e03f0685401a0394c821c4b70f1eccd657319
Akilesh A S <akileshas.ai@gmail.com>
2026-01-07 01:56:46 +0530

    feat: update action

--- hooks/server/mavproxy/_action.py
+++ hooks/server/mavproxy/_action.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import pandas as pd
 
 sys.path.extend([
   os.path.abspath(os.path.join(os.path.dirname(__file__), ".")),
@@ -9,8 +10,18 @@ sys.path.extend([
   os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")),
 ])
 
+from hooks.server.mavproxy._const import LL_STREAM_FILE
+
+def get_ll(ts):
+  data = pd.read_csv(LL_STREAM_FILE)
+  idx = (data["Timestamp"] - ts).abs().idxmin()
+  return (
+    data.at[idx, "Latitude"],
+    data.at[idx, "Longitude"],
+  )
+
 
 def do_action(
   timestamp,
 ):
-  time.sleep(10)
+  lat, long = get_ll(timestamp)
