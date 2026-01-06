f37607ce3a495f963b4efe0b4110a4d18a241f56
Akilesh A S <akileshas.ai@gmail.com>
2026-01-07 00:59:14 +0530

    feat: add placeholder for mavproxy

--- hooks/server/mavproxy/__init__.py
+++ hooks/server/mavproxy/__init__.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+sys.path.extend([
+  os.path.abspath(os.path.join(os.path.dirname(__file__), ".")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")),
+])
+
 from hooks.server.mavproxy._action import do_action
 
 
--- hooks/server/mavproxy/_action.py
+++ hooks/server/mavproxy/_action.py
@@ -1,2 +1,16 @@
-def do_action():
-  pass
+import os
+import sys
+import time
+
+sys.path.extend([
+  os.path.abspath(os.path.join(os.path.dirname(__file__), ".")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")),
+])
+
+
+def do_action(
+  timestamp,
+):
+  time.sleep(10)
