f253cb14c8d725b9273dfaf657407634a57e2901
Akilesh A S <akileshas.ai@gmail.com>
2026-01-06 14:23:00 +0530

    fix: module calls

--- model/train.py
+++ model/train.py
@@ -1,27 +1,41 @@
 import os
 import sys
 
-from ultralytics import YOLO
+from pathlib import Path
+
 
 COCO_DATASET = os.path.abspath(os.path.join(
   os.path.dirname(__file__),
   "./dataset/coco/data.yaml",
 ))
-WEIGHTS_PATH = os.path.abspath(os.path.join(
+PRETRAINED_WEIGHTS_DIR = os.path.abspath(os.path.join(
   os.path.dirname(__file__),
-  "./weights/"
+  "./weights/pretrained/"
 ))
 YOLO11M = os.path.abspath(os.path.join(
-  WEIGHTS_PATH,
-  "pretrained/yolo11m.pt"
+  os.path.dirname(__file__),
+  "./weights/pretrained/yolo11m.pt"
+))
+RUNNER_LOG_PATH = os.path.abspath(os.path.join(
+  os.path.dirname(__file__),
+  "./weights/run/"
 ))
 
+Path(PRETRAINED_WEIGHTS_DIR).mkdir(parents=True, exist_ok=True)
+Path(RUNNER_LOG_PATH).mkdir(parents=True, exist_ok=True)
+
+
+from ultralytics.utils import SETTINGS
+SETTINGS["weights_dir"] = PRETRAINED_WEIGHTS_DIR
+
+from ultralytics import YOLO
 model = YOLO(YOLO11M)
 model.train(
   data=COCO_DATASET,
   epochs=500,
+  batch=32,
   imgsz=1024,
   plots=True,
-  project=WEIGHTS_PATH,
-  name="trainOne",
+  project=RUNNER_LOG_PATH,
+  name="trainner",
 )
