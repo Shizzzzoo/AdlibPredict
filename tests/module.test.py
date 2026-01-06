508efe3fa04f5269d180bc6ffc65741bf6f9d108
Akilesh A S <akileshas.ai@gmail.com>
2026-01-06 06:06:35 +0530

    fix: add runtime

--- tests/module.test.py
+++ tests/module.test.py
@@ -1,9 +1,18 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+import sys
+
+sys.path.extend([
+    os.path.abspath(os.path.join(os.path.dirname(__file__), ".")),
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),
+])
+
+
 from adlibpredict import FrameCollector
-from .const import (
-  CHECK_INTERVAL,
-  RTSP_URL,
-)
 
 rec = FrameCollector(
-  rtsp_url="",
+  rtsp_url="rtsp://192.168.0.5:8554/live",
 )
