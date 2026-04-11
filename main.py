5dbaed953ef00bbe114d3d2f21f03d96213751d1
Akilesh A S <akileshas.ai@gmail.com>
2026-04-11 18:22:41 +0530

    fix: fix the workflow loop

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ load_dotenv()
 import os
 import sys
 import time
+import argparse
 
 from rich import print
 from pathlib import Path
@@ -17,21 +18,37 @@ from adlibpredict import (
 def workflow(
   test=False
 ):
-  rtsp_url = os.environ.get("RTSP_URL", "rtsp://localhost:8554/live")
-  interval_str = os.environ.get("CHECK_INTERVAL", "1.0")
+  if test:
+    rtsp_url = "rtsp://localhost:8554/live"
+  else:
+    rtsp_url = os.environ.get("RTSP_URL")
+    if not rtsp_url:
+      print("environment variable `RTSP_URL` does not exist.")
+  interval_str = os.environ.get(
+    "CHECK_INTERVAL",
+    "1.0",
+  )
   model_path = os.environ.get("MODEL_PATH")
   if not model_path:
-    print("environment variable `MODEL_PATH` does not exists.")
-    exit()
+    print("environment variable `MODEL_PATH` does not exist.")
+    sys.exit(1)
   if not Path(model_path).exists():
     print(f"model path: `{model_path}` does not exists.")
-    exit()
-  col = FrameCollector(rtsp_url)
+    sys.exit(1)
   try:
     interval = float(interval_str)
+    if interval <= 0:
+      raise ValueError("interval must be positive.")
   except ValueError:
     print(f"interval string must be a number, but got `{interval_str}`.")
-    exit()
+    sys.exit()
+  print(type(rtsp_url))
+  print(f"rtsp url: `{rtsp_url}`")
+  print(f"model path: `{model_path}`")
+  print(f"interval: `{interval}`")
+  col = FrameCollector(
+    rtsp_url=rtsp_url,
+  )
   det = Detector(model_path)
   det.load()
   while True:
@@ -45,10 +62,10 @@ def workflow(
           frame,
           frame_ts,
         )
-        print(res)
+        print(f"detection result: {res}")
         if res != -1.0:
           send_trigger(res)
-          print("sended.")
+          print("trigger sent.")
         elapsed = time.perf_counter() - loop_start
         time.sleep(max(0, interval - elapsed))
     except KeyboardInterrupt:
@@ -56,11 +73,19 @@ def workflow(
       break
     except Exception as exc:
       print(f"exception: {exc}")
-      time.sleep(exc)
+      time.sleep(1)
 
 
 def main():
-  workflow()
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    "--test",
+    action="store_true",
+  )
+  args = parser.parse_args()
+  workflow(
+    test=args.test,
+  )
 
 
 if __name__ == "__main__":
