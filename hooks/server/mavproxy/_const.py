0ced59fe8f8725229e5fc80a6fce9e451406a28e
Akilesh A S <akileshas.ai@gmail.com>
2026-04-11 18:40:51 +0530

    fix: update ll stream file path

--- hooks/server/mavproxy/_const.py
+++ hooks/server/mavproxy/_const.py
@@ -5,7 +5,8 @@ from rich import print
 from pathlib import Path
 from platformdirs import user_cache_dir
 
-LL_STREAM_FILE = Path(user_cache_dir(
-  appname="nidar",
-  ensure_exists=True,
-)) / "tll.csv"
+# LL_STREAM_FILE = Path(user_cache_dir(
+#   appname="nidar",
+#   ensure_exists=True,
+# )) / "tll.csv"
+LL_STREAM_FILE = Path(r"C:\Users\Dell\AppData\Local\nidar\tll.csv")
